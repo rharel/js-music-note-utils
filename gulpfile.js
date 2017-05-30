a2d2af24efc8fade61d762825ab7a74e894bc54f
Raoul Harel <raoul.harel@gmail.com>
2017-05-30 20:09:56 +0200

    move tests to node

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 const gulp = require("gulp");
 const babel = require("gulp-babel");
+const mocha = require("gulp-mocha");
 const rename = require("gulp-rename");
 const uglify = require("gulp-uglify");
 
-gulp.task("compile", function ()
+gulp.task("compile", function()
 {
 	return gulp.src("src/music_note_utils.es6")
 		.pipe(babel())
 		.pipe(gulp.dest("dist"));
 });
-gulp.task("build", ["compile"], function()
+gulp.task("test", function()
+{
+	return gulp.src("test/*.test.js", { read: false })
+		.pipe(mocha());
+});
+gulp.task("build", ["compile", "test"], function()
 {
 	return gulp.src("dist/music_note_utils.js")
 		.pipe(uglify())
